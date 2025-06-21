51e3cb5ed91dfe13202608a0465e82cba02f55cc
harsh <harsh.reactdev@gmail.com>
2025-06-21 13:57:46 +0530

    chore: started with c basics

---
+++ fundamentals/main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int a, b;
+    printf("Enter value for a : ");
+    scanf("%d", &a);
+    printf("Enter value for b : ");
+    scanf("%d", &b);
+
+    printf("The sum is %d\n", a + b);
+    return 0;
+}
